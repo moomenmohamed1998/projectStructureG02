bd3d0579cc6fd3a930d1d9b43deaec3ca37c46b1
moomen mohammmed <monm73530@gmail.com>
2025-03-08 19:36:47 +0200

    project

--- projectStructureG02/Startup.cs
+++ projectStructureG02/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing.Constraints;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -12,13 +13,17 @@ namespace projectStructureG02
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
+            //services.AddRazorPages();
+            //services.AddControllers();
+            //services.AddSignalR();
+            //services.AddAuthentication();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -34,6 +39,15 @@ namespace projectStructureG02
                 {
                     await context.Response.WriteAsync("Hello World!");
                 });
+                endpoints.MapControllerRoute
+                (
+                    name: "default",
+                    pattern: "{controller=Movies}/{action=Index}/{id:int?}",
+                //pattern: "{controller}/{action}/{id?}"
+                //defaults: new { controller = "Movies", action = "Index" }
+                constraints: new { id = new IntRouteConstraint() }
+
+                );
             });
         }
     }
